39d81d5616ca33a09cd13c6a06837914a81ac6d9
Ruan Valente <ruanopensuselinux@gmail.com>
2020-06-16 00:39:35 -0300

    listing and filtering feature by uf, city and items :tada:

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,6 +9,8 @@ const itemsController = new ItemsController();
 const pointsConroller = new PointsController();
 
 routes.get('/items', itemsController.index);
+routes.get('/points', pointsConroller.index);
+routes.get('/points/:id', pointsConroller.show);
 routes.post('/points', pointsConroller.create);
 
 export default routes;
